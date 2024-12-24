f5e20c4bce91685b3ed9ef93003ccc26adba6d3c
unknown <salhi197>
2024-12-24 12:52:42 +0100

    add tawk assistance

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -566,6 +566,17 @@ for (var i = 0; i < bubblyButtons.length; i++) {
   bubblyButtons[i].addEventListener('click', animateButton, false);
 }
 </script>
+<script type="text/javascript">
+  var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/6767278f49e2fd8dfefba3a2/1iflf52kk';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+  })();
+</script>
+
 
 </body>
 </html>
